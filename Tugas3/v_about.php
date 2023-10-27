d3ccf5a8fcc86734a004c4358cd1b00df6514000
rachyar <rifqiachyar30@gmail.com>
2023-10-27 16:38:57 +0700

    Tugas3

---
+++ Tugas3/v_about.php
@@ -0,0 +1,23 @@
+<section> 
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4> 
+    <ul type="disc"> 
+        <li>Nama Depan : Handrianus</li> 
+        <li>Nama Belakang : Wibowo</li> 
+    </ul> 
+    <br> 
+    <h4>Alamat</h4> 
+    <ul type="none"> 
+        <li> Jalan teuku cik ditiro rt 03 rw 09 karanglo madegondo solobaru</li> 
+    </ul> 
+    <h4>Tempat Lahir</h4> 
+    <ul type="none"> 
+        <li>Karanganyar</li> 
+    </ul> 
+    
+    <h4>Olah Raga Favorit</h4> 
+    <ul type="square"> 
+        <li>Sepedaan</li> 
+        <li>Lari</li> 
+    </ul> 
+</section>
